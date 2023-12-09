023abc1fcef894818b89dcf481410b6686e53e6f
Juan Hoyos <juanhoyos1347@gmail.com>
2023-12-09 18:54:07 -0500

    image and verifiy jwt

--- src/middlewares/verify-file.js
+++ src/middlewares/verify-file.js
@@ -2,16 +2,12 @@ const { response } = require("express")
 
 
 const verifyFile = (req, res = response, next ) => {
-
-    
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.file ) {
         return res.status(400).json({
             msg: 'No hay archivos que subir - verifyFile'
         });
     }
-
     next();
-
 }
 
 
